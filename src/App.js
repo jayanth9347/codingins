5ea4e19442c4bab5a9a8a06235a1fb79ce22c484
jayanth <pujalajayanth@9347>
2024-04-27 16:13:51 +0000

    commit messge

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import UserProfile from './components/UserProfile/index'
+
+const UserDetails = {
+  ImgeUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+  name: 'Jayanth',
+  role: 'Software Engineer',
+}
+
+const App = () => <UserProfile />
+
+export default App
